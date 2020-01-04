2999c60f27a2d885945e80192fc0128256138dce
Eunhak Lee <admin@return0927.xyz>
2020-01-05 02:32:28 +0900

    Impl: test/std_register.py: Register GPS address

--- Socket Middleware/test/std_register.py
+++ Socket Middleware/test/std_register.py
@@ -5,10 +5,14 @@ sock = socket.socket()
 sock.connect(("localhost", 8080))
 
 # --------------------------------
-# Register
+# Register Hostname
 sock.sendall("R:1234567890123456".encode())
-
 msg, data = sock.recv(2**12).decode().split(":", 1)
 print("Recv", msg, data)
 
-sock.sendall("C:".encode())
+input("Enter to publish GPS")
+
+# Register GPS
+sock.sendall("G:1234567890123456:37.490762,126.8844066".encode())
+msg, data = sock.recv(2**12).decode().split(":", 1)
+print("Recv", msg, data)
