8cf91a50baff49735e00631b488e97baa974d15a
Eunhak Lee <admin@return0927.xyz>
2020-01-05 10:45:35 +0900

    Impl: app.py: Add backend transaction with event data

--- Socket Middleware/app.py
+++ Socket Middleware/app.py
@@ -94,6 +94,15 @@ class SocketReceiver:
                 device_id, data = data.split(":", 1)
                 self.logger.warning(f"Host {device_id}: Incoming event data")
 
+                hostname, percentage = data.split(":", 1)
+                percentage = float(percentage)
+
+                async with aiohttp.ClientSession(headers={"Content-Type": "application/x-www-form-urlencoded"}) as sess:
+                    async with sess.post("http://r.kdw.kr:9999/{}/event".format(device_id),
+                                         data="db={}".format(percentage)) as resp:
+                        data = await resp.text()
+                        print("Backend respose:", data)
+                return await self.write(reader, writer, b"S:OK")
         except:
             return await self.write(reader, writer, b"E:Unk")
 
